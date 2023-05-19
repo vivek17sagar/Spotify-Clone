ea6b78a6a5ea703e1c130e5f6fd258f1c7b4b1ca
Vivek Sagar <viveksagar7319@gmail.com>
2023-05-19 16:44:37 +0530

    updated login and signup

---
+++ jsComponents/login.js
@@ -0,0 +1,31 @@
+
+const userInputLogin = document.querySelector('.username-1');
+const userInputPassword = document.querySelector('.password-1');
+
+         
+
+document.querySelector('.lgn-btn-1').addEventListener('click',()=>{
+    console.log(arrayOfUser)
+    arrayOfUser.forEach((item)=>{
+        if(userInputLogin.value==item.username){
+            if(userInputPassword.value==item.password){
+                document.querySelector('.main-container-sign').style.display = "none"
+                document.querySelector('.main-container-1').style.display = "none"
+                document.querySelector('.global-container').style.display = "block"
+            }else{
+                document.querySelector('.lgn-error').innerHTML = "Password is incorrect !!";
+                document.querySelector('.lgn-error').style.display = "block"
+            }
+        }else{
+            document.querySelector('.lgn-error').innerHTML = "User Not Found !!";
+            document.querySelector('.lgn-error').style.display = "block"
+        }
+    })
+})
+
+
+document.querySelector('.signup').addEventListener('click',()=>{
+    document.querySelector('.main-container-1').style.display = "none"
+    document.querySelector('.global-container').style.display = "none"
+    document.querySelector('.main-container-sign').style.display = "block"
+})
---
+++ jsComponents/signup.js
@@ -0,0 +1,55 @@
+const name = document.querySelector('.name');
+const username = document.querySelector('.username');
+const password = document.querySelector('.password');
+
+
+let arrayOfUser = JSON.parse(localStorage.getItem('user'))
+
+document.querySelector('.lgn-btn').addEventListener('click',()=>{
+   verifyUser();
+   arrayOfUser.push({
+        name:name.value,username:username.value,password:password.value
+    })
+
+
+    localStorage.setItem(`user`,JSON.stringify(arrayOfUser))
+
+    let getArray = JSON.parse(localStorage.getItem('user'));
+
+    arrayOfUser = [...getArray]
+
+    // console.log(JSON.parse(localStorage.getItem('user')))
+    name.value = ''
+    username.value = ''
+    password.value = ''
+})
+
+
+
+function verifyUser(){
+    
+    arrayOfUser.forEach((item)=>{
+        if(name.value==item.name){
+            document.querySelector('.error').innerHTML = 'Name already exist !!'
+            document.querySelector('.error').style.color = "red";
+            document.querySelector('.error').style.display = 'block'
+        }
+        else if(username.value==item.username){
+            document.querySelector('.error').innerHTML = 'username already exist !!'
+            document.querySelector('.error').style.color = "red";
+            document.querySelector('.error').style.display = 'block'
+        }else{
+            document.querySelector('.error').innerHTML = 'Successful !!'
+            document.querySelector('.error').style.color = "green";
+            document.querySelector('.error').style.display = 'block'
+        }
+    })
+}
+
+
+
+document.querySelector('.lgn').addEventListener('click',()=>{
+    document.querySelector('.main-container-1').style.display = "block"
+    document.querySelector('.global-container').style.display = "none"
+    document.querySelector('.main-container-sign').style.display = "none"
+})
